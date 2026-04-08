08088dd1fb911017d63c2283c9018ece01869a4c
Nick K <cspios@gmail.com>
2026-04-08 17:56:14 +0300

    batman

---
+++ src/main/java/pada/aay/AayApplication.java
@@ -0,0 +1,13 @@
+package pada.aay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pada/aay/AayApplicationTests.java
@@ -0,0 +1,13 @@
+package pada.aay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
